38d31bffdb35a32f1a68a24b10c6f757130530e7
see-k <flabioski@gmail.com>
2020-10-18 23:41:54 -0500

    Adding Shared Code And Server G

--- gameServer/Server.c
+++ gameServer/Server.c
@@ -10,71 +10,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include "Practical.h"
+#include "sharedCode.h"
 
 static const int MAXPENDING = 5; // Maximum outstanding connection requests
 
-//Fuction: Set up TCP Server Socket
-int SetupTCPServerSocket(const char* service) 
-{
-    // Construct the server address structure
-    struct addrinfo addrCriteria; // Criteria for address match
-    memset(&addrCriteria, 0, sizeof(addrCriteria)); // Zero out structure
-    addrCriteria.ai_family = AF_UNSPEC; // Any address family
-    addrCriteria.ai_flags = AI_PASSIVE; // Accept on any address/port
-    addrCriteria.ai_socktype = SOCK_STREAM; // Only stream sockets
-    addrCriteria.ai_protocol = IPPROTO_TCP; // Only TCP protocol
-
-    struct addrinfo* servAddr; // List of server addresses
-    int rtnVal = getaddrinfo(NULL, service, &addrCriteria, &servAddr);
-    if (rtnVal != 0)
-        DieWithUserMessage("getaddrinfo() failed", gai_strerror(rtnVal));
-
-    int servSock = -1;
-    for (struct addrinfo* addr = servAddr; addr != NULL; addr = addr->ai_next) {
-     // Create a TCP socket
-        servSock = socket(servAddr->ai_family, servAddr->ai_socktype,
-        servAddr->ai_protocol);
-        if (servSock < 0)
-            continue; // Socket creation failed; try next address
-      
-          // Bind to the local address and set socket to list
-        if ((bind(servSock, servAddr->ai_addr, servAddr->ai_addrlen) == 0) && (listen(servSock, MAXPENDING) == 0)) 
-        {
-            // Print local address of socket
-            struct sockaddr_storage localAddr;
-            socklen_t addrSize = sizeof(localAddr);
-            if (getsockname(servSock, (struct sockaddr*)&localAddr, &addrSize) < 0)
-                DieWithSystemMessage("getsockname() failed");
-            fputs("Binding to ", stdout);
-            PrintSocketAddress((struct sockaddr*)&localAddr, stdout);
-            fputc('\n', stdout);
-            break; // Bind and list successful
-            
-        }
-        close(servSock); // Close and try again
-        servSock = -1;
-    }
-    // Free address list allocated by getaddrinfo()
-     freeaddrinfo(servAddr);
-     return servSock;
-}
-
-//Function Die With User Message
-void DieWithUserMessage(const char* msg, const char* detail) {
-      fputs(msg, stderr);
-      fputs(": ", stderr);
-      fputs(detail, stderr);
-      fputc('\n', stderr);
-      exit(1);
-
-}
-
-//Function Die With User Message
-void DieWithSystemMessage(const char* msg) {
-        perror(msg);
-        exit(1);
-}
 
 
 //Fuction: HandleTCP Client
@@ -103,7 +42,6 @@ void HandleTCPClient(int clntSocket) {
 
     }
 }
-
     
 int main(int argc, char* argv[])
 {
@@ -141,7 +79,8 @@ int main(int argc, char* argv[])
          close(clntSock); // Parent closes child socket descriptor
          childProcCount++; // Increment number of child processes
         
-             while (childProcCount) { // Clean up all zombies
+         while (childProcCount)
+         { // Clean up all zombies
              processID = waitpid((pid_t)-1, NULL, WNOHANG); // Non-blocking wait
              if (processID < 0) // waitpid() error?
                  DieWithSystemMessage("waitpid() failed");
@@ -149,8 +88,8 @@ int main(int argc, char* argv[])
                  break;
              else
                  childProcCount--; // Cleaned up after a child
-            
-        }
+         }
+        
         
     }
     // NOT REACHED
